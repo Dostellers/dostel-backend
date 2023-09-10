f2e609fe2f3cfb4fb3ca09fd73a75fcfe8292689
Sushil Kumar Singh <sushil@cvtrobotics.in>
2023-09-10 18:53:34 +0530

    Added Resolvers and typedefs

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-const app = require('./src'); // Import the Express app from src/index.js
-const { logger } = require('./src/config/logger');
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const bodyParser = require('body-parser');
+const { logger, connectDB, typeDefs, resolvers } = require('./src');
+
+const app = express();
+
+// Use body parser
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Initialize Apollo Server
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+        return {
+            // user: ...
+        };
+    }
+});
+
+// Apply Apollo GraphQL middleware and set the path to /graphql
+server.applyMiddleware({ app, path: '/graphql' });
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    logger.info(`Server started on http://localhost:${PORT}`);
+    logger.info(`Server is running on http://localhost:${PORT}${server.graphqlPath}`);
 });
-
--- src/index.js
+++ src/index.js
@@ -1,50 +1,12 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const { config } = require('./config');
-const { logger } = require('./config/logger');
+const logger = require('./config/logger');
+const connectDB = require('./config/dbConfig');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
-// Import typeDefs and resolvers
-const typeDefs = require('./schema/typeDefs');
-const resolvers = require('./resolvers'); // Assuming you have an index.js in resolvers that exports all resolvers
-
-const app = express();
-
-// Use body parser
-app.use(bodyParser.json());
-
-// Connect to MongoDB
-mongoose.connect(config.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-}).then(() => {
-  logger.info('MongoDB connected successfully.');
-}).catch((error) => {
-  logger.error('Error connecting to MongoDB: ', error);
-});
-
-// Initialize Apollo Server
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => {
-    // Here you can extract user information from the request, for authentication/authorization purposes
-    return {
-      // user: ...
-    };
-  }
-});
-
-// Apply Apollo GraphQL middleware and set the path to /graphql
-server.applyMiddleware({ app, path: '/graphql' });
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  logger.info(`Server is running on http://localhost:${PORT}${server.graphqlPath}`);
-});
-
-module.exports = app;
+module.exports = {
+    logger,
+    connectDB,
+    typeDefs,
+    resolvers
+    // ... any other utilities or configurations you want to export
+};
